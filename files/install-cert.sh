a90bf0d0b8f6f6eb78346ee4e98c7c50e2899d54
MM <mm@c-base.org>
2016-11-16 14:29:58 +0100

    update parameters of install-cert.sh script

--- files/install-cert.sh
+++ files/install-cert.sh
@@ -2,7 +2,7 @@
 
 . /etc/dehydrated/config
 
-DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" CHAINFILE="${4}"
+DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}" TIMESTAMP="${6}"
 
 # This file is called once for each certificate that has been
 # produced. Here you might, for instance, copy your new certificates
@@ -16,8 +16,12 @@ DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" CHAINFILE="${4}"
 #   The path of the file containing the private key.
 # - CERTFILE
 #   The path of the file containing the signed certificate.
+# - FULLCHAINFILE
+#   The path of the file containing the full certificate chain.
 # - CHAINFILE
 #   The path of the file containing the full certificate chain.
+# - TIMESTAMP
+#   Timestamp when the specified certificate was created.
 
 echo " ! Certificate installation not yet configured"
 echo " ! edit /etc/dehydrated/install-cert.sh"
@@ -27,6 +31,8 @@ exit 1
 # example
 #cat "$CHAINFILE" > /etc/ssl/certs/cert.pem
 #cat "$KEYFILE" > /etc/ssl/private/key.pem
+#chown root.ssl-cert /etc/ssl/private/key.pem
+#chmod 640 /etc/ssl/private/key.pem
 #
 #/usr/sbin/service nginx reload
 #/usr/sbin/service exim4 restart
